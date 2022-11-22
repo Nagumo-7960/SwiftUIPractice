4c2084911e0e6e4413915ee6a83432ba6b6939ba
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-11-22 23:48:49 +0900

    ボタンを押した時にコンソール表示

---
+++ SwiftUIPractice/SwiftUIPractice/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUIPractice
+//
+//  Created by なぐも on 2022/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button("ボタン"){
+                print("ボタンが押された")
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
