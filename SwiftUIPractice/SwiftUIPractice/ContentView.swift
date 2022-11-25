53348ee8ea27dc6824754d4b4559560647d4cb7f
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-11-25 20:27:58 +0900

    おみくじを引くためのボタンを作成

--- SwiftUIPractice/SwiftUIPractice/ContentView.swift
+++ SwiftUIPractice/SwiftUIPractice/ContentView.swift
@@ -10,19 +10,14 @@ import SwiftUI
 struct ContentView: View {
     @State var text = "Hello, world!"
     var body: some View {
-    
-            NavigationView{
-                VStack{
-                    Text("画面1")
-                        .padding()
-                    NavigationLink(destination: SecondView().navigationTitle("画面2")){
-                        Text("画面2へ")
-                    }
+        VStack{
+            Button(action: {
+                print("おみくじを引いた")}){
+                    Text("おみくじを引く")
                 }
-                .navigationTitle("画面1")
-            }
         }
     }
+}
 
 
 struct ContentView_Previews: PreviewProvider {
