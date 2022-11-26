45e2fec0914936cdb84928e8dc2ad432579897b2
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-11-26 19:13:06 +0900

    おみくじの結果を表示

--- SwiftUIPractice/SwiftUIPractice/ContentView.swift
+++ SwiftUIPractice/SwiftUIPractice/ContentView.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var text = "Hello, world!"
+    @State var result = ["大吉", "中吉", "吉", "凶"]
     var body: some View {
         VStack{
+            Text(result.first!).font(.largeTitle)
+                .padding()
+            
             Button(action: {
                 print("おみくじを引いた")}){
                     Text("おみくじを引く")
